b72d06f7891ce59d1d8d9c3d501c6eaee169b856
Vaclav Novotny <vaclav@easymarkit.com>
2017-07-10 16:15:58 +0200

    Init commit

---
+++ Assets/scripts/PlayerController.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+var rb: Rigidbody;
+var speed = 10;
+var score = 0;
+var scoreText;
+
+function Start() {
+	rb = GetComponent.<Rigidbody>();
+}
+
+function FixedUpdate() {
+	var moveHorizontal = Input.GetAxis('Horizontal');
+	var moveVertical = Input.GetAxis('Vertical');
+
+	var vector = new Vector3(moveHorizontal, 0, moveVertical);
+
+	rb.AddForce(vector * speed);
+}
+
+function OnTriggerEnter(other: Collider) {
+	if (other.gameObject.CompareTag('Pick Up')) {
+		other.gameObject.SetActive(false);
+
+		score++;
+		Debug.Log(score);
+	}
+}
